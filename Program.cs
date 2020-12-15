8a94f30410ab21a7a4f65ed3d7a1a5d55d23e452
St2r <St2r@github.com>
2020-12-15 17:02:42 +0800

    :tada: init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace compiler_c0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
